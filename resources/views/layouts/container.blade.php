3ec1701489202a404c5064371ed824b2e3a7a17e
younes talibi <younessetalibi11@gmail.com>
2023-12-21 09:04:46 +0100

    'update'

--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -181,10 +181,10 @@
                     <footer class="content-footer footer bg-footer-theme">
                         <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
                             <div class="mb-2 mb-md-0 ">
-                                Copyright &copy; 2023 
-                                <div style="font-size:small">
-                                     made by <a href="https://younes-talibi.web.app/">younes-talibi</a>
-                                </div>
+                            Copyright &copy; 2023 Handymen To Call All Rights Reserved.
+                                <!-- <div style="font-size:small">
+                                    made by <a href="https://younes-talibi.web.app/">younes-talibi</a>
+                                </div> -->
                             </div>
                             
                         </div>
